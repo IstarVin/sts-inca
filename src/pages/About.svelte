2448804f6f8f4c7e3253db2814647cd03b7a436c
IstarVin <ajdeleverio20@gmail.com>
2024-09-28 12:59:19 +0800

    update

--- src/pages/About.svelte
+++ src/pages/About.svelte
@@ -1,6 +1,1 @@
 <h1>About</h1>
-<img
-    src="http://localhost:3000/images/bg1.jpeg"
-    alt="background"
-    class="fixed top-0 w-full h-full object-cover z-[-4] opacity-80"
-/>
--- src/pages/Home.svelte
+++ src/pages/Home.svelte
@@ -1,6 +1,1 @@
 <h1>Home</h1>
-<img
-    src="http://localhost:3000/images/bg0.jpeg"
-    alt="background"
-    class="fixed top-0 w-full h-full object-cover z-[-4] opacity-80"
-/>
